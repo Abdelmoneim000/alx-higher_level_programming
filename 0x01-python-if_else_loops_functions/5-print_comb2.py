a03858d30374c9dd6f006291c88dd63a0226dc4b
Abdelmoneim000 <ab000arabi22@gmail.com>
2023-06-06 11:15:55 -0700

    print all numbers from 0 to 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if i == 99:
+    i = str(i)
+    if int(i) < 10:
+        i = "0" + i
+    if i == "99":
         print("{}\n".format(i), end='')
     else:
         print("{}, ".format(i), end='')
