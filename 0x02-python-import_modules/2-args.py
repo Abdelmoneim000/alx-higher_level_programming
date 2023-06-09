9b40c6fbeb04f01f5ab229d9c46ab0d37166f008
Abdelmoneim000 <ab000arabi22@gmail.com>
2023-06-09 07:43:13 -0700

    finish task 2 to make the code interactive

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+from sys import argv
+leng = len(argv) -1
+
+numb = 1
+
+for i in argv:
+    if i == argv[0]:
+        print("{0} arguments:".format(leng))
+    else:
+        print("{0}: {1}".format(numb, i))
+        numb += 1
