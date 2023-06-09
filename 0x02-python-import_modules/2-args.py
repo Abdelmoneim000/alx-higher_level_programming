a0817be1c19740a0ec9e06458aebba414397d32a
Abdelmoneim000 <ab000arabi22@gmail.com>
2023-06-09 17:50:24 +0300

    fix the module error

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-from sys import argv
-leng = len(argv) -1
-
-numb = 1
-
-for i in argv:
-    if i == argv[0]:
-        print("{0} arguments:".format(leng))
-    else:
-        print("{0}: {1}".format(numb, i))
-        numb += 1
+if __name__ == "__main__" :
+    from sys import argv
+    leng = len(argv) -1
+    
+    numb = 1
+    
+    for i in argv:
+        if i == argv[0]:
+            print("{0} arguments:".format(leng))
+        else:
+            print("{0}: {1}".format(numb, i))
+            numb += 1
