6f4e6e8408063c1aa76e8347449503baa4039ba6
Abdelmoneim000 <ab000arabi22@gmail.com>
2023-06-09 17:23:44 +0300

    finish task 1 import functions making an exotic way to solve this task by making a list of the functions and for loop over them

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    my_func = [add, sub, mul, div]
+    operator = ["+", "-", "*", "/"]
+    index = 0
+a = 10
+b = 5
+
+for i in my_func:
+    print("{0} {1} {2}= {3} ".format(a, operator[index], b, i(a, b)))
+    index += 1
