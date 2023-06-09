9548952044aae4ec5e2a5a47fa9a927b515d5ce5
Abdelmoneim000 <ab000arabi22@gmail.com>
2023-06-09 18:01:18 +0300

    fix the typo in the output

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,9 +5,15 @@ if __name__ == "__main__" :
     
     numb = 1
     
-    for i in argv:
-        if i == argv[0]:
-            print("{0} arguments:".format(leng))
-        else:
-            print("{0}: {1}".format(numb, i))
-            numb += 1
+    if leng == 0:
+        print("{0} arguments.".format(leng))
+    elif leng == 1:
+        print("{0} argument:\n{1}: {2}".format(leng, numb, argv[numb]))
+    else:
+        print("{0} arguments:".format(leng))
+        for i in argv:
+            if i == argv[1]:
+                continue
+            else:
+                print("{0}: {1}".format(numb, argv[numb]))
+                numb += 1
