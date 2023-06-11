544cd172532c43de15811e0706ae6c03380a6df4
Abdelmoneim000 <ab000arabi22@gmail.com>
2023-06-11 18:59:05 +0300

    finish task 6 print matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,10 @@
-#!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if not matrix:
         pass
     else:
-        for i in matrix:
-            for j in i:
-                print("{:d} ".format(j), end="")
-            print()
+        for row in matrix:
+            for element in row:
+                print("{:d}".format(element), end="")
+                if element != row[-1]:
+                    print(" ", end="")
+            print()  # prints a newline after each row
