7475005771a77b5fac62e8e34f465f5c4b4bc781
Abdelmoneim000 <ab000arabi22@gmail.com>
2023-06-11 20:06:21 +0300

    finish task 8 create new tuple with length andchar

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    new_t = (len(sentence), sentence[0])
+    if len(sentence):
+        new_t = (len(sentence), sentence[0])
+    else:
+        new_t = (0, "None")
     return new_t
