9e7b8b60be2f7d68add5b54d82f6f009c7b6d684
Abdelmoneim000 <ab000arabi22@gmail.com>
2023-06-11 20:22:31 +0300

    finish task 9 find the biggest integer in a list

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list:
+        counter = my_list[0]
+        for i in my_list:
+            if i > counter:
+                counter = i
+            else:
+                continue
+        return counter
+    
