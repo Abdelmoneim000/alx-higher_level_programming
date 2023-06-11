113db6190563c8ee064ae6c33a270cf63526d41b
Abdelmoneim000 <ab000arabi22@gmail.com>
2023-06-11 19:56:16 +0300

    doing the hard thing

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,19 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) > len(tuple_b):
-        new_t = list(tuple_a)
-    else:
-        new_t = list(tuple_b)
-    if len(tuple_a) > len(tuple_b):
-        for i in range(len(new_t)):
-            if len(tuple_b) - 1 < i:
-                new_t[i] = new_t[i] + 0
-            else:
-                new_t[i] = new_t[i] + tuple_b[i]
-    else:
-        for i in range(len(new_t)):
-            if len(tuple_a) - 1 < i:
-                new_t[i] = new_t[i] + 0
-            else:
-                new_t[i] = new_t[i] + tuple_a[i]
-    return tuple(new_t)
+    new_tuple = ()
+    tuple_1 = tuple_a + (0, 0)
+    tuple_2 = tuple_b + (0, 0)
+    new_tuple = tuple_1[0] + tuple_2[0], tuple_1[1] + tuple_2[1]
+    return new_tuple
