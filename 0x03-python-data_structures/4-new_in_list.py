a60385b08783ea6950ab3569d66546f04a0d0779
Abdelmoneim000 <ab000arabi22@gmail.com>
2023-06-11 17:57:44 +0300

    finish task 4 copy and print a list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    new_list = my_list
+    list_new = []
+    for i in my_list:
+        list_new.append(i)
     if idx > len(my_list) - 1 or idx < 0:
-        return new_list
+        return list_new
     else:
-        new_list[idx] = element
-        return new_list
+        list_new[idx] = element
+        return list_new
