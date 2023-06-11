3572d51e540cf0e31896201ec4c36ddfe56817d9
Abdelmoneim000 <ab000arabi22@gmail.com>
2023-06-11 20:56:17 +0300

    finish task 11 delete specific element from a list

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,5 +3,10 @@ def delete_at(my_list=[], idx=0):
     if idx > len(my_list) - 1 or idx < 0:
         return my_list
     else:
-        new_list = my_list[:idx] + my_list[idx + 1:]
+        new_list = []
+        for i in range(len(my_list)):
+            if i == idx:
+                continue
+            else:
+                new_list.append(my_list[i])
         return new_list
