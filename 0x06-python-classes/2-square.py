1066c1bd106e14d7abac6e4e6007a469f05a5338
Abdelmoneim000 <ab000arabi22@gmail.com>
2023-07-01 11:16:42 +0300

    finish task 2 square attribute size

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""A module to export a square with size private attribute"""
+
+
+class Square:
+    """A blue print for a square"""
+
+    def __init__(self, size=0):
+        """
+        initializing a new instance.
+
+        Args:
+        size (int): The size of the new square.
+        """
+        if (type(size) != int):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
