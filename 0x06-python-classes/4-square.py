7bbd4f875963d39d9a154bf9e88908a692cefabd
Abdelmoneim000 <ab000arabi22@gmail.com>
2023-07-01 11:41:40 +0300

    finish task 4 - add properties

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""A module to export a square with size private attribute"""
+
+
+class Square:
+    """A blue print for a square"""
+
+    def __init__(self, size=0):
+        """
+        initializing a new instance.
+
+        Args:
+        size (int): The size of the new square.
+        """
+        self.__size = size
+
+    def area(self):
+        """
+        A method that returns the area of the square
+        """
+        return (self.__size * self.__size)
+
+    @property
+    def size(self):
+        """A property to retrieve the size private attribute"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if (type(value) != int):
+            raise TypeError("size must be an integer")
+        elif (value < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = value
