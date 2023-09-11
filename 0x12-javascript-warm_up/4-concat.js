9eab45d9caf67b01e74f1894cf76d5b05d2faa7e
Abdelmoneim000 <ab000arabi22@gmail.com>
2023-09-11 13:37:31 +0300

    fix first line

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,4 +1,4 @@
-#!/home/abod/.nvm/versions/node/v18.17.1/bin/node
+#!/usr/bin/node
 const pro = require("process");
 const first = `${pro.argv[2]} is ${pro.argv[3]}`;
 first ? console.log(first) : console.log("No argument");
