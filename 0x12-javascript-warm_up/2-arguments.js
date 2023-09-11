4fb138063a071f91e26827517178628c80dec0b9
Abdelmoneim000 <ab000arabi22@gmail.com>
2023-09-11 13:13:01 +0300

    try

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,5 @@
 #!/usr/bin/node
-const pro = require('process')
-pro.argv.length <= 2 ? console.log("No argument") : console.log("Argument found");
+const pro = require("process");
+pro.argv.length <= 1
+  ? console.log("No argument")
+  : console.log("Argument found");
