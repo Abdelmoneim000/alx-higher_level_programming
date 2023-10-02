5c39523f35b77a674c82c82af312d465e639f8a2
Abdelmoneim000 <ab000arabi22@gmail.com>
2023-10-03 02:34:08 +0300

    filter states

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,10 +11,8 @@ if __name__ == '__main__':
                          db=sys.argv[3], port=3306)
 
     cur = db.cursor()
-    cur.execute("SELECT * \
-    FROM states \
-    WHERE CONVERT(`name` USING Latin1) \
-    COLLATE Latin1_General_CS = '{}';".format(sys.argv[4]))
+    query = "SELECT * FROM states WHERE `name`='{name:s}';".format(name=sys.argv[4])
+    cur.execute(query)
     states = cur.fetchall()
 
     for state in states:
