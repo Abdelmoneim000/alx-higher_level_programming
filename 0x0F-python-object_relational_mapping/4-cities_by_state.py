8967c5402702a146d85b108a002cdd4104d8c750
Abdelmoneim000 <ab000arabi22@gmail.com>
2023-10-03 03:39:19 +0300

    fix pycodestyle

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -7,20 +7,20 @@ import sys
 import MySQLdb
 
 if __name__ == '__main__':
-	db = MySQLdb.Connect(user=sys.argv[1], passwd=sys.argv[2],
-						 db=sys.argv[3], port=3306)
-	cursor = db.cursor()
-	query = (
+    db = MySQLdb.Connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
+    cursor = db.cursor()
+    query = (
         "SELECT cities.id, "
         "cities.name, "
         "states.name "
         "FROM cities "
         "JOIN states ON states.id=cities.state_id"
     )
-	cursor.execute(query)
-	cities = cursor.fetchall()
+    cursor.execute(query)
+    cities = cursor.fetchall()
 
-	for city in cities:
-		print(city)
-	cursor.close()
-	db.close()
+    for city in cities:
+        print(city)
+    cursor.close()
+    db.close()
