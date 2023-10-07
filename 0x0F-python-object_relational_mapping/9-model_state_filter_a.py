92e304c94343a326bdd897d2c175c2f50d72cc00
Abdelmoneim000 <ab000arabi22@gmail.com>
2023-10-07 06:50:31 +0300

    finish task 9 print filtered output from database

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+A script to connect to the database
+and get filter rows from the state table
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine, MetaData, Column, String, Integer
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    from sqlalchemy.orm import sessionmaker
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(State.name.like('%a%')).order_by(State.id)
+
+    for i in state:
+        print("{}: {}".format(i.id, i.name))
+
+
+    session.close()
