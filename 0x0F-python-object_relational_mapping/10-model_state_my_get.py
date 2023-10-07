70befe111edff99b0fb42d2d7d3f11a03a4751c4
Abdelmoneim000 <ab000arabi22@gmail.com>
2023-10-07 07:01:57 +0300

    finish task 10 get the id of the matching city

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+A script to connect to the database
+and get filter rows from the state table
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine, MetaData, Column, String, Integer
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    from sqlalchemy.orm import sessionmaker
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(State.name.like(sys.argv[4])).first()
+
+    if state is not None:
+        print("{}".format(state.id))
+    else:
+        print("Nothing")
+
+    session.close()
