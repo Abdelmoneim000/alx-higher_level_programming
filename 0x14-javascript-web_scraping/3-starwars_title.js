2145f286f75ad02ed21277836239ee736dae9b22
Abdelmoneim000 <ab000arabi22@gmail.com>
2023-10-24 17:19:13 +0300

    finish task 3 making a request in js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,8 +1,8 @@
-#!/usr/bin/env node
+#!/usr/bin/node
 const request = require('request');
 const { argv } = require('process');
 
 request.get(`https://swapi-api.alx-tools.com/api/films/${argv[2]}/`,
-  (error, response) => {
-    error ? console.log(error) : console.log(`${JSON.parse(response.body).title}`);
-  });
+(error, response) => {
+  error ? console.log(error) : console.log(`${JSON.parse(response.body).title}`);
+});
