64fc5f14cfb5a0f88e5429abc651981b481295cd
Abdelmoneim000 <ab000arabi22@gmail.com>
2023-10-24 12:15:25 +0300

    finish task 0 read a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,6 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 const fs = require('fs');
-const {argv} = require('process');
+const { argv } = require('process');
 fs.readFile(argv[2], 'utf8', function (error, content) {
-    console.log(error || content);
-})
+  console.log(error || content);
+});
