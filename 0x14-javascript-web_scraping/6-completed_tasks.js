3ac141f7e93140da742f992af0a30929de23b434
Abdelmoneim000 <ab000arabi22@gmail.com>
2023-10-24 19:28:04 +0300

    finish task 6 counting completed tasks for users

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-const fs = require('fs');
 const { argv } = require('process');
 const request = require('request');
 
@@ -7,20 +6,18 @@ const request = require('request');
 const url = argv[2];
 
 // Declare the object for user Tasks
-let obj = {}
+const obj = {};
 // Send a GET request to the API
 request.get(url, (err, response) => {
   if (err) {
     throw err;
   } else {
     const bod = JSON.parse(response.body);
-    for(let i of bod)
-    {
-        if(i['completed'] === true)
-        {
-        const userId = i['userId'].toString(); // Convert userId to a string
+    for (const i of bod) {
+      if (i.completed === true) {
+        const userId = i.userId.toString(); // Convert userId to a string
         obj[userId] = (obj[userId] || 0) + 1; // Initialize to 0 if undefined, then increment
-        }
+      }
     }
   }
   console.log(obj);
