4f88dda13481e7dbef9c4958c79289b0fdf0febe
Abdelmoneim000 <ab000arabi22@gmail.com>
2023-10-24 18:46:58 +0300

    finish task 5 saving  the body of the response into a file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -8,15 +8,14 @@ const url = argv[2];
 
 // Send a GET request to the API
 request.get(url, (err, response) => {
-    if(err)
-    {
-        throw err;
-    }
-    else
-    {
-        const bod = response.body;
-        fs.writeFile(argv[3], bod, (err) => {
-            err ? console.log(err) : err;
-        });
-    }
+  if (err) {
+    throw err;
+  } else {
+    const bod = response.body;
+    fs.writeFile(argv[3], bod, (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
 });
